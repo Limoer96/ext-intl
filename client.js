45c9a71eb51f73fbd415276b45e8344c3d47ee83
Limoer96 <myt19960503@outlook.com>
2020-05-25 09:21:05 +0800

    [*]修改cli，兼容新流程

--- client.js
+++ client.js
@@ -1,23 +1,25 @@
 #!/usr/bin/env node
 
-const args = process.argv.slice(2)
-const intl = require('./dist/index.js')
+// const args = process.argv.slice(2)
+const { intl } = require('./dist/index.js')
 
-function _findIndex(args) {
-  let expected = ['--config', '-p']
-  for (let i = 0; i < args.length; i += 1) {
-    if (expected.indexOf(args[i]) > -1) {
-      return i
-    }
-  }
-  return -1
-}
+intl()
 
-const index = _findIndex(args)
+// function _findIndex(args) {
+//   let expected = ['--config', '-p']
+//   for (let i = 0; i < args.length; i += 1) {
+//     if (expected.indexOf(args[i]) > -1) {
+//       return i
+//     }
+//   }
+//   return -1
+// }
 
-if (index > -1) {
-  let params = [...args]
-  const configPath = params[1]
-  const config = require(configPath)
-  intl.traverse(config)
-}
+// const index = _findIndex(args)
+
+// if (index > -1) {
+//   let params = [...args]
+//   const configPath = params[1]
+//   const config = require(configPath)
+//   intl.traverse(config)
+// }
